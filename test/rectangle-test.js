d260b5ab33f984c95684d43d9d6addfaab3d5e3c
Sam Landfried <samlandfried@gmail.com>
2017-06-26 17:06:23 -0600

    Test rectangles

---
+++ test/rectangle-test.js
@@ -0,0 +1,22 @@
+const assert = require('chai').assert;
+const rectangle = require('../diagonal');
+
+describe('Rectangle', () => {
+  const rec = { width: 10, height: 10 };
+
+  it('#area', () => {
+    assert.equal(rectangle.area(rec), 100);
+  });
+
+  it('#diagonal', () => {
+    assert.equal(rectangle.diagonal(rec), 14.142135623730951);
+  });
+
+  it('#perimeter', () => {
+    assert.equal(rectangle.perimeter(rec), 40);
+  });
+
+  it('#isSquare', () => {
+    assert.isTrue(rectangle.perimeter(rec));
+  });
+});
